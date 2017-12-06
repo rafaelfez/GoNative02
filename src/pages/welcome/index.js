8bd3488f990c9b70c4074bf640b2698a43877a24
Rafael Felipe <rafafelipesilva@gmail.com>
2017-12-05 22:55:06 -0200

    Estilizando

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import styles from './styles';
 
 export default class Welcome extends Component {
-    render() {
-        return (
-            <View />
-        );
-    }
+  static navigationOptions = {
+    header: null,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcomeTitle}>Bem-vindo</Text>
+        <Text style={styles.welcomeDescription}>
+          Para continuar, precisamos que você informe seu usuário no Github
+        </Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu usuário"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Text style={styles.buttonText}>Prosseguir</Text>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
 }
---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  white: '#FFF',
+  light: '#EEE',
+  dark: '#666',
+  darker: '#444',
+
+  inactive: '#877EA0',
+
+  primary: '#444A5A',
+  secundary: '#7A91CA',
+
+  background: 'F8FAFF',
+};
